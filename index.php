4f2b31bb54b706d294617a679c4dac0b20e43165
Chris-Håvard Berge <chb@oclin.no>
2020-04-24 08:16:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-
+<html>
+  
+  <head>
+    
+    <title>ServerSentEvents</title>
+    
+  </head>
+  
+  <body>
+    
+    <div id="result"></div>
+     
+    <script>
+      if(typeof(EventSource) !== "undefined") {
+        var source = new EventSource("sync.php");
+        source.onmessage = function(event) {
+          document.getElementById("result").innerHTML = event.data + "<br>";
+        };
+        } else {
+          document.getElementById("result").innerHTML = "Sorry, your browser does not support server-sent events...";
+        }
+    </script>
+    
+  </body>
+  
+</html>
